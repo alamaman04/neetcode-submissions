34569762e1086caa2c6ca4dfba7d923931256b1b
Md Aman <153177857+alamaman04@users.noreply.github.com>
2026-06-18 23:53:42 +0530

    Add: rotting-fruit - submission-2

---
+++ Data Structures & Algorithms/rotting-fruit/submission-2.cpp
@@ -0,0 +1,50 @@
+class Solution {
+   public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+
+        queue<pair<int, int>> q;
+        
+        int fresh = 0 ;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(grid[i][j]==1) fresh++;
+                if (grid[i][j] == 2) {
+                    q.push({i, j});
+                    vis[i][j] = true ;
+                }
+            }
+        }
+        // check if no fruits 
+        if(fresh==0) return 0 ;
+
+        int dx[] = {1, -1, 0, 0};
+        int dy[] = {0, 0, -1, 1};
+        int steps = 0 ;
+        while (!q.empty()) {
+            int l = q.size();
+            bool rottenAny = false;
+            for (int i = 0; i < l; i++) {
+                auto p = q.front();
+                q.pop();
+                int r = p.first;
+                int c = p.second;
+                for (int d = 0; d < 4; d++) {
+                    int nr = r + dx[d];
+                    int nc = c + dy[d];
+                    if (nr < 0 || nr >= m || nc < 0 || nc >= n || grid[nr][nc] != 1 || vis[nr][nc])
+                        continue;
+                    vis[nr][nc] = true ;
+                    q.push({nr,nc}) ;
+                    fresh-- ;
+                    rottenAny = true ;
+                }
+            }
+            if(rottenAny)steps++ ;
+        }
+        if(fresh>0) return -1;
+        return steps ;
+    }
+};
