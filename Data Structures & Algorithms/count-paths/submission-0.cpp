6b6fe3d9e14fbb12eebc4e19a293ba807a22a003
Md Aman <153177857+alamaman04@users.noreply.github.com>
2026-07-15 04:56:02 +0400

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m,vector<int>(n,0)) ;
+        
+        for(int i=0 ;i<m ;i++) dp[i][0] = 1 ;
+        for(int j=0 ;j<n ;j++) dp[0][j] = 1 ;
+
+        auto valid = [&](int r ,int c){
+            return r>=0 && r<m && c>=0 && c<n ;
+        };
+
+        for(int i=1 ;i<m ;i++){
+            for(int j=1 ;j<n;j++){
+                dp[i][j] += dp[i-1][j] ;
+                dp[i][j] += dp[i][j-1] ;
+            }
+        }
+        return dp[m-1][n-1] ;
+    }
+};
